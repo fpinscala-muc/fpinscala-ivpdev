d012bfbe1979e28065e9a1929d96d9ea69134c65
ivpdev <ivp@nomail.no>
2014-11-18 21:19:17 +0100

    chapter 6: exercises up to 6.9

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -76,19 +76,45 @@ object RNG {
     go(rng, count)
   }
 
-  def doubleViaMap: Rand[Double] = ???
+  def doubleViaMap: Rand[Double] = {
+    map(int) { i =>
+      Math.abs(i.toDouble) / (Int.MaxValue.toDouble + 2)
+    }
+  }
 
-  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
+  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = {
+    rng => {
+      val (a, rng1) = ra(rng)
+      val (b, rng2) = rb(rng1)
+      (f(a,b), rng2)
+    }
+  }
 
-  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = ???
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = {
+    fs.foldRight(unit(List()): Rand[List[A]]){ (ra, rla)  => map2(ra, rla)(_ :: _) }
+  }
 
-  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = ???
+  def flatMap[A,B](ra: Rand[A])(g: A => Rand[B]): Rand[B] = {
+    rng => {
+      val (a, rng1) = ra(rng)
+      val rb = g(a)
+      rb(rng1)
+    }
+  }
 
-  def nonNegativeLessThan(n: Int): Rand[Int] = ???
+  def nonNegativeLessThan(n: Int): Rand[Int] = {
+    map(int){
+      i => ((Math.abs(i.toDouble) / (Int.MaxValue.toDouble + 2)) * n).toInt
+    }
+  }
 
-  def mapViaFlatMap[A,B](s: Rand[A])(f: A => B): Rand[B] = ???
+  def mapViaFlatMap[A,B](s: Rand[A])(f: A => B): Rand[B] = flatMap(s)(a => unit(f(a)))
 
-  def map2ViaFlatMap[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
+  def map2ViaFlatMap[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = {
+    flatMap(ra){ a =>
+      flatMap(rb) { b => unit(f(a,b)) }
+    }
+  }
 }
 
 case class State[S,+A](run: S => (A, S)) {
